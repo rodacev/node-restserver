2d560fcb2ce10b3e8780dfc23342cbe01f06f5ea
Rodrigo <rodacev@gmail.com>
2020-12-18 12:07:28 -0300

    bariable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://rodacev:VENfLVJlIukf7WT1@cluster0.uizgn.mongodb.net/cafe'
+    urlDB = process.env.mongo_uri;
 }
 
 
